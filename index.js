4d4b3ce3faa9113a095319ac5365389a3606d3f7
Shubham Lal <lal.shubham027@gmail.com>
2023-10-09 23:55:00 +0530

    restructure files

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.use('/api/status', (req, res) => {
 });
 
 // Frontend Routes
-app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')))
+app.use(express.static(path.resolve(__dirname, 'client', 'dist')))
 app.get('*', (req, res) => {
     res.sendFile(
-        path.resolve(__dirname, '..', 'client', 'dist', 'index.html'),
+        path.resolve(__dirname, 'client', 'dist', 'index.html'),
         function (err) {
             if (err) {
                 res.status(500).send(err)
