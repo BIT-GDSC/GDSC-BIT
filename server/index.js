42d400e21c404e15fafec07febbac316d30c5c9c
Shubham Lal <lal.shubham027@gmail.com>
2023-10-09 23:03:35 +0530

    server setup

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json({ limit: '25mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cors());
+
+// Backend Routes
+app.use('/api/status', (req, res) => {
+    res.send("Server Running!");
+});
+
+// Frontend Routes
+app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')))
+app.get('*', (req, res) => {
+    res.sendFile(
+        path.resolve(__dirname, '..', 'client', 'dist', 'index.html'),
+        function (err) {
+            if (err) {
+                res.status(500).send(err)
+            }
+        }
+    )
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Connected to PORT => ${PORT}`);
+});
