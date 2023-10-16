f87c107f8d77113b943a7f0865f63a25bacb372b
Shubham Lal <lal.shubham027@gmail.com>
2023-10-16 21:41:24 +0530

    client: add proxy to Vite Configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  },
 })
