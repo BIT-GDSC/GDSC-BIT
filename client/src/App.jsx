851b2627810068009fc1f58edce2b3cf623de41c
Jay9874 <jayprakashsharma225@gmail.com>
2023-11-03 23:27:14 +0530

    fix caching

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,54 @@
-import './App.css';
-import { useEffect } from 'react';
-import { Routes, Route, useLocation } from "react-router-dom";
-import Container from './components/Container';
-import Protected from './pages/Protected';
-import Home from './pages/Home';
-import Redirect from './pages/Redirect';
-import VerifyCertificate from './pages/Certificate/VerifyCertificate';
-import Auth from './pages/Auth';
-import Test from './pages/Test';
-import { useLoginStore } from './store/useAuthStore';
+import './App.css'
+import { useEffect } from 'react'
+import { Analytics } from '@vercel/analytics/react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Container from './components/Container'
+import Protected from './pages/Protected'
+import Home from './pages/Home'
+import Redirect from './pages/Redirect'
+import VerifyCertificate from './pages/Certificate/VerifyCertificate'
+import Auth from './pages/Auth'
+import Test from './pages/Test'
+import { useLoginStore } from './store/useAuthStore'
 
-function App() {
-  const location = useLocation();
-  const { loadUser } = useLoginStore();
+function App () {
+  const location = useLocation()
+  const { loadUser } = useLoginStore()
 
   useEffect(() => {
-    if (location.pathname !== '/auth') loadUser();
-  }, [location.pathname]);
-
+    if (location.pathname !== '/auth') loadUser()
+  }, [location.pathname])
 
   return (
     <>
       <Container>
         <Routes>
-          <Route path="/auth" element={<Auth />} />
+          <Route path='/auth' element={<Auth />} />
 
-          <Route path="/test" element={<Protected />}>
+          <Route path='/test' element={<Protected />}>
             <Route index element={<Test />} />
           </Route>
 
-          <Route path="/" element={<Home />} />
+          <Route path='/' element={<Home />} />
 
-          <Route exact path="/events/pokeprompt" element={<Redirect route="game" />} />
+          <Route
+            exact
+            path='/events/pokeprompt'
+            element={<Redirect route='game' />}
+          />
 
-          <Route exact path="/events/latest" element={<Redirect route="latest" />} />
+          <Route
+            exact
+            path='/events/latest'
+            element={<Redirect route='latest' />}
+          />
 
-          <Route path="/cert/verify/:certificateID" element={<VerifyCertificate />} />
+          <Route
+            path='/cert/verify/:certificateID'
+            element={<VerifyCertificate />}
+          />
         </Routes>
+        <Analytics />
       </Container>
     </>
   )
