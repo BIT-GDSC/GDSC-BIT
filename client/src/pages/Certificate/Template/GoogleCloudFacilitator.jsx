852cb9df50c1955a89f3b2d2b50fa960e04aee87
Jay Prakash Sharma <jayprakashsharma225@gmail.com>
2023-11-04 13:22:42 +0530

    Update GoogleCloudFacilitator.jsx
    add LinkedIn docs procedure to add certificate.

--- client/src/pages/Certificate/Template/GoogleCloudFacilitator.jsx
+++ client/src/pages/Certificate/Template/GoogleCloudFacilitator.jsx
@@ -40,12 +40,19 @@ const GoogleCloudFacilitator = () => {
                     >
                         Share on X
                     </a>
-                    <a
+                    <div><a href="https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=Test%
+20Certificate&organizationId=1337&issueYear
+=2018&issueMonth=2&expirationYear=2020
+&expirationMonth=5&certUrl=https%3A%2F%
+2Fdocs.microsoft.com%2Fen-us%2Flearn%2Fcertifications%2Fd365-functional-consultant-sales&certId=1234"><img src="https://download.linkedin.com/desktop/add
+2profile/buttons/en_US.png " alt="LinkedIn Add to Profile button"></a></div>
+                        {/*<a
                         href={`https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=${certData.certificate}&organizationId=97886448&issueYear=2023&issueMonth=10&certId=${certData._id}&certUrl=${certData.verifyURL}`}
                         className='rounded-2xl w-[150px] h-[35px] bg-[#0072b1] hover:bg-white text-white hover:text-[#0072b1] duration-200 font-extrabold flex items-center justify-center'
                     >
                         Add to LinkedIn
                     </a>
+                        */}
                 </div>
                 <PDFDownloadLink
                     document={<GoogleCloudFacilitatorPDF certData={certData} />}
