3fb6ec607d4de078367c8ff10d6af1d4e1730a0b
Jay9874 <jayprakashsharma225@gmail.com>
2023-11-06 23:37:53 +0530

    Client: add mobile menu arrow

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,22 @@
-import { Link } from 'react-router-dom';
-import { navData } from '../utils/navbar';
-import { useAuthStore } from '../store/useAuthStore';
+import { Link } from 'react-router-dom'
+import { navData } from '../utils/navbar'
+import { useAuthStore } from '../store/useAuthStore'
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
+import { useEffect, useState } from 'react'
 
 export const Navbar = () => {
-  const { verifyLoading, verifySuccess, user } = useAuthStore();
+  const { verifyLoading, verifySuccess, user } = useAuthStore()
+  const [activeNav, setActiveNav] = useState(false)
 
   return (
     <div className='Navbar-container'>
       <div className='Navbar-flex-container'>
-        <Link to="/" className='Navbar-logo'>
-          <img src="/logo.png" alt='logo' />
+        <Link to='/' className='Navbar-logo'>
+          <img src='/logo.png' alt='logo' />
         </Link>
         <div className='Navbar-links'>
-          {navData.map((item) => (
+          {navData.map(item => (
             <Link to={item.link} className='Navbar-link' key={item.id}>
               <span>{item.name}</span>
               <div className='Navbar-link-highlight' />
@@ -23,7 +25,7 @@ export const Navbar = () => {
         </div>
         <div className='Navbar-auth'>
           {!verifyLoading && !verifySuccess ? (
-            <Link to="/auth" className='Navbar-signin-button'>
+            <Link to='/auth' className='Navbar-signin-button'>
               Sign in
             </Link>
           ) : !verifyLoading && verifySuccess ? (
@@ -38,10 +40,24 @@ export const Navbar = () => {
               <Skeleton className='absolute -top-1 left-0 right-0 bottom-0 w-full h-full' />
             </div>
           )}
-        </div>
-        <div className='Navbar-menu-toggler'>
-          <div className='Navbar-menu-toggler-top'></div>
-          <div className='Navbar-menu-toggler-bottom'></div>
+          {/* Mobile Menu toggler */}
+          <div
+            className='Navbar-menu-toggler'
+            onClick={() => setActiveNav(!activeNav)}
+          >
+            <div className='Navbar-menu-bar-container'>
+              <div
+                className={`Navbar-menu-toggler-top arrow-bar ${
+                  activeNav && 'active'
+                }`}
+              ></div>
+              <div
+                className={`Navbar-menu-toggler-bottom ${
+                  activeNav && 'active'
+                } arrow-bar`}
+              ></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
