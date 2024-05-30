ce7e26ed4cd8c50ec27ea4c332d7ab2e826b08ff
Jay9874 <jayprakashsharma225@gmail.com>
2024-05-30 23:44:11 +0530

    client: improve mobile menu view

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -18,8 +18,8 @@ const Container = ({ children }) => {
         {location.pathname !== '/auth' && <Navbar />}
         <div className='w-full h-full fixed -z-10 top-0 left-0 right-0 bg-gradient-to-b from-[#FFBC39] to-[#FFF]' />
         <div>{children}</div>
-        {menuPopup && <MobileMenu menuLinks={navData} />}
-        {avatarPopup && <AvatarMenu />}
+        {/* {menuPopup && <MobileMenu menuLinks={navData} />}
+        {avatarPopup && <AvatarMenu />} */}
       </div>
     </>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 export const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false)
   const { width } = useWindowDimension()
   const { verifyLoading, verifySuccess, user } = useAuthStore()
   const {
@@ -24,12 +25,12 @@ export const Navbar = () => {
   } = useAnimStore()
 
   function handleMenuToggle() {
+    setDropdown(!dropdown)
     console.log('clicked on menu')
-    if (mobileMenu){
+    if (mobileMenu) {
       closeMenu()
       setMobileMenu(false)
-    } 
-    else{
+    } else {
       openMenu()
       setMobileMenu(true)
     }
@@ -56,62 +57,80 @@ export const Navbar = () => {
 
   return (
     <div className='Navbar-container'>
-      <div className='Navbar-flex-container'>
-        <Link to='/' className='Navbar-logo'>
-          <img src='/logo-crop.png' alt='logo' />
-        </Link>
-        <div className='Navbar-links'>
-          {navData.map((item) => (
-            <Link to={item.link} className='Navbar-link' key={item.id}>
-              <span>{item.name}</span>
-              <div className='Navbar-link-highlight' />
-            </Link>
-          ))}
-        </div>
-        <div className='Navbar-auth'>
-          {verifyLoading ? (
-            <div className='relative w-[40px] h-[40px] rounded-full  overflow-hidden'>
-              <Skeleton className='absolute -top-1 left-0 right-0 bottom-0 w-full h-full' />
-            </div>
-          ) : !verifyLoading && !verifySuccess ? (
-            <Link to='/auth' className='Navbar-signin-button'>
-              Sign in
-            </Link>
-          ) : (
-            !verifyLoading &&
-            verifySuccess && (
-              <div
-                className='w-[40px] h-[40px] rounded-full border border-[#3c82f6] bg-white overflow-hidden cursor-pointer'
-                onClick={() => handleAvatarToggle()}
-              >
-                <img
-                  src={user?.avatar?.url ? user.avatar.url : '/user.svg'}
-                  className='w-full h-full object-contain'
-                />
+      <div className='navbar-inner-cont'>
+        <div className='Navbar-flex-container'>
+          <Link to='/' className='Navbar-logo'>
+            <img src='/logo-crop.png' alt='logo' />
+          </Link>
+          <div className='Navbar-links'>
+            {navData.map((item) => (
+              <Link to={item.link} className='Navbar-link' key={item.id}>
+                <span>{item.name}</span>
+                <div className='Navbar-link-highlight' />
+              </Link>
+            ))}
+          </div>
+          <div className='Navbar-auth'>
+            {verifyLoading ? (
+              <div className='relative w-[40px] h-[40px] rounded-full  overflow-hidden'>
+                <Skeleton className='absolute -top-1 left-0 right-0 bottom-0 w-full h-full' />
               </div>
-            )
-          )}
-          {/* Mobile Menu toggler */}
-          <button
-            disabled={toggleDisable}
-            className='Navbar-menu-toggler'
-            onClick={() => handleMenuToggle()}
-          >
-            <div
-              className={`Navbar-menu-bar-container ${mobileMenu && 'active'}`}
+            ) : !verifyLoading && !verifySuccess ? (
+              <Link to='/auth' className='Navbar-signin-button'>
+                Sign in
+              </Link>
+            ) : (
+              !verifyLoading &&
+              verifySuccess && (
+                <div
+                  className='w-[40px] h-[40px] rounded-full border border-[#3c82f6] bg-white overflow-hidden cursor-pointer'
+                  onClick={() => handleAvatarToggle()}
+                >
+                  <img
+                    src={user?.avatar?.url ? user.avatar.url : '/user.svg'}
+                    className='w-full h-full object-contain'
+                  />
+                </div>
+              )
+            )}
+            {/* Mobile Menu toggler */}
+            <button
+              disabled={toggleDisable}
+              className='Navbar-menu-toggler'
+              onClick={() => handleMenuToggle()}
             >
               <div
-                className={`Navbar-menu-toggler-top arrow-bar ${
+                className={`Navbar-menu-bar-container ${
                   mobileMenu && 'active'
                 }`}
-              ></div>
-              <div
-                className={`Navbar-menu-toggler-bottom ${
-                  mobileMenu && 'active'
-                } arrow-bar`}
-              ></div>
-            </div>
-          </button>
+              >
+                <div
+                  className={`Navbar-menu-toggler-top arrow-bar ${
+                    mobileMenu && 'active'
+                  }`}
+                ></div>
+                <div
+                  className={`Navbar-menu-toggler-bottom ${
+                    mobileMenu && 'active'
+                  } arrow-bar`}
+                ></div>
+              </div>
+            </button>
+          </div>
+        </div>
+
+        {/* Experimet with nav */}
+        <div className={`mobile-dropdown ${dropdown ? 'active' : ''}`}>
+          <ul className='mobile-links-container'>
+            {navData.map((item, index) => (
+              <li key={index}>
+                <Link to={item.link} className='Navbar-link' key={item.id}>
+                  <span>{item.name}</span>
+                  <div className='Navbar-link-highlight' />
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
