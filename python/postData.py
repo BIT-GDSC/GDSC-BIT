343de65c8358417db2e3f9664d5677604760ae86
Jay9874 <jayprakashsharma225@gmail.com>
2024-08-21 09:54:21 +0530

    add python module and core cert

---
+++ python/postData.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from pymongo import MongoClient
+
+
+data = pd.read_csv('core_members.csv')
+
+json_data = data.to_dict(orient='records')
+db_uri = os.getenv('MONGO_URI')
+print("Connecting to Database...")
+client = MongoClient(db_uri)
+db = client['test']
+collection = db['certificates']
+print("Database connected successfully!")
+
+print("Modifying Database...")
+for row in json_data:
+    collection.update_one({'_id': row.get('_id')}, {'$set': row}, upsert=True)
+print("Database modified successfully!")
