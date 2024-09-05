210cb029892331ee69982c0fc339e1b628c7a5f9
Jay9874 <jayprakashsharma225@gmail.com>
2024-09-06 00:04:51 +0530

    update vite build config to remove versioning

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' //removed swc
-import { version } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     headers: {
       'Strict-Transport-Security': 'max-age=86400; includeSubDomains', // Adds HSTS options to your website, with a expiry time of 1 day
       'X-Content-Type-Options': 'nosniff', // Protects from improper scripts runnings
@@ -20,13 +18,4 @@ export default defineConfig({
       }
     }
   },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].${version}.js`,
-        chunkFileNames: `[name].${version}.js`,
-        assetFileNames: `[name].${version}.[ext]`
-      }
-    }
-  }
 })
